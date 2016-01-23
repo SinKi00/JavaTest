4a52ccb9b3f44771f9848c393f705ab5f351bab2
zhipshao <zhipshao@163.com>
2016-01-23 18:38:29 +0800

    this change by bran1

--- BranchTest.java
+++ BranchTest.java
@@ -1,6 +1,6 @@
 
 public class BranchTest {
     public static void main(String[] args){
-    	System.out.println("Hello Branch1!:");
+    	System.out.println("Hello Branch1!----googbye");
     }
 }
